590f58ec439aef1c775d0184244cb74a2370e5fe
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2025-01-19 20:06:15 +0200

    Sound for buttons and inputs

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,60 +1,29 @@
-const inputFile = document.querySelector('#detroit-json-label');
-const inputText = document.querySelector('.container-search__container-input__input');
-
 const selectSound = document.querySelector('#selectSound');
 const openSound = document.querySelector('#openSound');
 
-const informationKeyButton = document.querySelector('.containter-information-key__button');
-const tableButtons = document.querySelectorAll('.tableButton');
-const informationKeyInputs = document.querySelectorAll('.containter-information-key__container-input__input');
-const filterButton = document.querySelector('.container-filter__button');
-const filterSelects = document.querySelectorAll('.container-filter__content__select__label');
+const inputs = document.querySelectorAll('input');
+const buttons = document.querySelectorAll('button');
 
-inputFile.addEventListener('mouseover', () => {
-    selectSound.currentTime=0;
-    selectSound.play();
-});
-
-inputFile.addEventListener('click', () => {
-    openSound.currentTime=0;
-    openSound.play();
-});
-
-initInputSound(inputFile);
-initInputSound(inputText);
-initButtonSound(informationKeyButton);
-initButtonSound(filterButton);
-tableButtons.forEach(btn => {
-    initButtonSound(btn);
-});
-informationKeyInputs.forEach(input => {
-    initInputSound(input);
-});
-filterSelects.forEach(select => {
-    initButtonSound(select);
-});
-
-function initButtonSound(button) {
-    button.addEventListener('click', () => {
+inputs.forEach(input => {
+    console.log('Set up sound for input',input)
+    input.addEventListener('mouseover', () => {
+        selectSound.currentTime = 0;
+        selectSound.play();
+    });
+    input.addEventListener('click', () => {
         openSound.currentTime = 0;
         openSound.play();
     });
+})
 
+buttons.forEach(button => {
+    console.log('Set up sound for button', button)
     button.addEventListener('mouseover', () => {
         selectSound.currentTime = 0;
         selectSound.play();
     });
-}
-
-function initInputSound(input) {
-    input.addEventListener('mouseover', () => {
-        selectSound.currentTime=0;
-        selectSound.play();
-    });
-    
-    input.addEventListener('focus', () => {
-        openSound.currentTime=0;
+    button.addEventListener('click', () => {
+        openSound.currentTime = 0;
         openSound.play();
     });
-}
-
+})
