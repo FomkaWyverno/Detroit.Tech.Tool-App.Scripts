379eda9f924bbf4493544d1cb1e1689274c6c7bc
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2025-01-19 19:31:23 +0200

    Created final layout

---
+++ scripts/animations.js
@@ -0,0 +1,35 @@
+const block_text = document.querySelector('.key_info_container');
+const wrapper_text = document.querySelector('.key_info_container__wrapper')
+
+function updateHeight() {
+    const computedStyle = window.getComputedStyle(block_text);
+    const paddingTop = parseFloat(computedStyle.paddingTop)
+    const paddingBottom = parseFloat(computedStyle.paddingBottom)
+
+    // Визначаємо поточну висоту контенту
+    const textHeight = wrapper_text.scrollHeight + paddingTop + paddingBottom;
+
+    if (textHeight > 315) {
+        block_text.style.height='315px';
+        console.log('height - 315px')
+    } else {
+        block_text.style.height = textHeight+'px'
+        console.log('current-height-'+textHeight)
+    }
+}
+
+const observer = new MutationObserver(() => {
+    updateHeight();
+})
+
+observer.observe(wrapper_text, {
+    childList: true,      // Відслідковуємо додавання або видалення дочірніх елементів
+    subtree: true,        // Відслідковуємо зміни в усіх нащадках
+    characterData: true   // Відслідковуємо зміни текстового вмісту
+})
+
+updateHeight()
+
+// setInterval(() => {
+//     document.querySelector('#text').textContent = 'Any Text '.repeat(Math.floor(Math.random() * 50))
+// }, 1500);
