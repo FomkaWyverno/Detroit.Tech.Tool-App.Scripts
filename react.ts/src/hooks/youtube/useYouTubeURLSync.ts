86de8db410dc08bc469f5e2303cc9da3d9d8442f
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2025-01-27 01:49:28 +0200

    optimize imports

--- react.ts/src/hooks/youtube/useYouTubeURLSync.ts
+++ react.ts/src/hooks/youtube/useYouTubeURLSync.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { YouTubeVideoPlayer } from "../../types/youtube";
 import { getVideoIdFromURL } from "../../utils/YouTubePlayerUtil";
 
