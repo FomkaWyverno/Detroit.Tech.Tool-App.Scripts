1fdaa15b1371582aaaaaa81da1027b2dba5940f8
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2025-02-01 00:07:30 +0200

    Fix animation infinity progress

--- react.ts/src/components/LoadingScreen/ProgressBar/ProgressBar.tsx
+++ react.ts/src/components/LoadingScreen/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import style from './ProgressBar.module.scss'
 
 interface IProgressBar {
@@ -10,12 +10,71 @@ function ProgressBar({
     width,
     progress
 }: IProgressBar) {
+    const fillRef = useRef<HTMLDivElement | null>(null);
+    const [translateX, setTranslateX] = useState<string>('translateX(0%)');
+
+    useEffect(() => { // Анімація безкінечного прогрессу
+        if (progress < 0 && fillRef.current) {
+            return animation_infinity_progress(fillRef, setTranslateX)
+        }
+    }, [progress]);
 
     return (
         <div className={style.progress_bar} style={{width: width}}>
-            <div className={`${style.progress_bar_fill} ${progress < 0 ? style.progress_bar_fill__infitity : ''}`} style={progress > -1 ? {width: width * progress} : {}}></div>
+            <div
+                className={
+                    `${style.progress_bar_fill}
+                     ${progress < 0 ? style.progress_bar_fill__infitity : ''}`}
+                
+                style={
+                    progress > -1 ? {width: width * progress} : {transform: translateX}}
+                ref={fillRef}
+                    ></div>
         </div>
     )
 }
 
 export default ProgressBar
+
+function animation_infinity_progress(
+    fillRef: React.MutableRefObject<HTMLDivElement | null>, 
+    setTranslateX: React.Dispatch<React.SetStateAction<string>>
+) {
+    const duration: number = getTransitionDurationInMs(fillRef.current!);
+    let startTime: number | null = null; 
+    console.log(`Start animation infinity progress ${duration}ms`);
+    const stepAnimation = (timeStamp: number) => {
+        if (!startTime) startTime = timeStamp;
+
+        const elapsed = timeStamp - startTime;
+
+        if (elapsed >= duration) {
+            setTranslateX((prev: string) => prev === 'translateX(0%)' ? 'translateX(calc(100% * 9))' : 'translateX(0%)');
+            startTime = timeStamp;
+        }
+
+        animationId = window.requestAnimationFrame(stepAnimation);
+    }
+
+    setTimeout(() => {
+        setTranslateX('translateX(calc(100% * 9))');
+    }, 0);
+
+    let animationId = window.requestAnimationFrame(stepAnimation);
+
+    return () => { // Функція для відміни анімації
+        window.cancelAnimationFrame(animationId);
+        setTranslateX('translateX(0%)');
+    }
+}
+
+function getTransitionDurationInMs(element: HTMLElement): number {
+    const computedStyle = window.getComputedStyle(element);
+    const duration = computedStyle.transitionDuration; // Наприклад, "0.3s"
+
+    const durationInMs = duration.includes('ms') 
+        ? parseFloat(duration) 
+        : parseFloat(duration) * 1000;
+
+    return durationInMs;
+}
