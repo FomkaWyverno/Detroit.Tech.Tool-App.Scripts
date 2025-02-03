d9ca6de7ccf7474e2462a976c27da9b33021689f
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2025-02-04 01:19:26 +0200

    Added new sounds

--- react.ts/src/utils/SoundManager.ts
+++ react.ts/src/utils/SoundManager.ts
@@ -1,44 +1,51 @@
 import hoverAudioFile from '../assets/sounds/hover.ogg';
 import clickAudioFile from '../assets/sounds/click.ogg';
 import errorAudioFile from '../assets/sounds/error.mp3';
+import error2AudioFile from '../assets/sounds/error2.mp3';
+import popAudioFile from '../assets/sounds/pop.mp3';
 
 class _SoundManager {
     private clickSound: HTMLAudioElement;
     private hoverSound: HTMLAudioElement;
     private errorSound: HTMLAudioElement;
+    private error2Sound: HTMLAudioElement;
+    private popSound: HTMLAudioElement;
 
     constructor() {
         this.clickSound = new Audio(clickAudioFile);
         this.hoverSound = new Audio(hoverAudioFile);
         this.errorSound = new Audio(errorAudioFile);
+        this.error2Sound = new Audio(error2AudioFile);
+        this.popSound = new Audio(popAudioFile);
     }
 
-    playClickSound() {
-        this.clickSound.currentTime = 0;
-        this.clickSound.play();
+    private playSound(sound: HTMLAudioElement, volume: number = 1) {
+        sound.currentTime = 0;
+        sound.volume = volume;
+        sound.play();
     }
 
-    playHoverSound() {
-        this.hoverSound.currentTime = 0;
-        this.hoverSound.play();
+    private playLoopSound(sound: HTMLAudioElement, volume: number = 1) {
+        sound.currentTime = 0;
+        sound.volume = volume;
+        sound.loop = true;
+        sound.play();
     }
 
-    playError() {
-        this.errorSound.currentTime = 0;
-        this.errorSound.play();
+    private stopLoopSound(sound: HTMLAudioElement) {
+        sound.pause();
+        sound.currentTime = 0;
+        sound.volume = 1;
+        sound.loop = false;
     }
 
-    playLoopError() {
-        this.errorSound.currentTime = 0;
-        this.errorSound.loop = true;
-        this.errorSound.play();
-    }
-
-    stopLoopError() {
-        this.errorSound.loop = false;
-        this.errorSound.pause();
-        this.errorSound.currentTime = 0;
-    }
+    playClickSound(volume: number = 1) {this.playSound(this.clickSound, volume);}
+    playHoverSound(volume: number = 1) {this.playSound(this.hoverSound, volume);}
+    playError(volume: number = 1) {this.playSound(this.errorSound, volume);}
+    playError2(volume: number = 1) {this.playSound(this.error2Sound, volume);}
+    playPop(volume: number = 1) {this.playSound(this.popSound, volume);}
+    playLoopError(volume: number = 1) {this.playLoopSound(this.errorSound, volume)}
+    stopLoopError() {this.stopLoopSound(this.errorSound);}
 }
 
 const SoundManager = new _SoundManager();
